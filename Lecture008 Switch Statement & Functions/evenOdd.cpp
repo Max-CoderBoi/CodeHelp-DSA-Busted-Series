6e37d2da5e02bb9bc6846119c4ff5b8bcc198e61
akash.patil <akashpatil.ap1610@gmail.com>
2025-02-15 20:12:04 +0530

    Update evenOdd.cpp
    🔹 Why This Version is Better?
    ✔ More Readable: Uses true and false instead of 0 and 1.
    ✔ More Compact: return !(a & 1); eliminates unnecessary if-else.
    ✔ Better User Experience: Adds "Enter a number: " prompt before input.

--- Lecture008 Switch Statement & Functions/evenOdd.cpp
+++ Lecture008 Switch Statement & Functions/evenOdd.cpp
@@ -1,32 +1,54 @@
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+
+
+// //1 -> Even
+// // 0 -> odd
+// bool isEven(int a) {
+//     //odd
+//     if(a&1) {
+//         return 0;
+//     }
+//     else { //Even
+//         return 1;
+//     }
+
+// }
+
+// int main() {
+    
+//     int num;
+//     cin >> num;
+
+//     if(isEven(num)) {
+//         cout <<" Number is Even" <<endl;
+//     }
+//     else {
+//         cout<< " Number is Odd" <<endl;
+//     }
+
+
+//     return 0;
+// }
+#include <iostream>
 using namespace std;
 
-
-//1 -> Even
-// 0 -> odd
+// Function to check even or odd
 bool isEven(int a) {
-    //odd
-    if(a&1) {
-        return 0;
-    }
-    else { //Even
-        return 1;
-    }
-
+    return !(a & 1);  // Returns true if even, false if odd
 }
 
 int main() {
-    
     int num;
+    
+    cout << "Enter a number: "; // Prompt for user input
     cin >> num;
 
-    if(isEven(num)) {
-        cout <<" Number is Even" <<endl;
+    if (isEven(num)) {
+        cout << "Number is Even" << endl;
+    } else {
+        cout << "Number is Odd" << endl;
     }
-    else {
-        cout<< " Number is Odd" <<endl;
-    }
-
 
     return 0;
 }
